7b6c7fb977b44b6c5a43cfbc02db788facac461c
Matt Hobbs <matt@nooshu.com>
2026-04-25 19:58:51 +0100

    Add Luma rendering support for additional fractal types
    - Introduced `renderLuma` functions in `burning-ship.js`, `celtic-mandelbrot.js`, `mutant-mandelbrot.js`, `phoenix-mandelbrot.js`, and `tricorn.js` to enable Luma rendering capabilities.
    - Integrated `createLumaDrawCommand` from `luma-utils.js` for enhanced rendering performance across these fractal modules.
    - Improved overall rendering consistency by standardizing the Luma rendering approach for 2D fractals.

--- static/js/fractals/2d/burning-ship.js
+++ static/js/fractals/2d/burning-ship.js
@@ -1,4 +1,5 @@
 import { createFragmentShader, createStandardDrawCommand } from '../utils.js';
+import { createLumaDrawCommand } from '../luma-utils.js';
 
 const fractalFunction = `
     float computeFractal(vec2 c) {
@@ -150,6 +151,10 @@ export function render(regl, params, canvas, options = {}) {
     ubo,
     juliaC: { x: 0, y: 0 }, // Not used for Burning Ship
   });
+}
+
+export function renderLuma(device, params, canvas) {
+  return createLumaDrawCommand(device, 'burning-ship', fractalFunction, params, canvas);
 }
 
 export const is2D = true;
